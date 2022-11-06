facfeee7627c0a925311e0d099ca6855f934334a
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-11-06 23:11:41 +0600

    cart cpmponet

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+
 import Product from '../Product/Product';
 import "./Shop.css"
 
@@ -33,9 +35,9 @@ const Shop = () => {
 
             </div>
             <div className="cart-container">
+                <Cart cart={cart}></Cart>
 
-                <h2>Order summery</h2>
-                <p>Selected Items: {cart.length}</p>
+
 
             </div>
 

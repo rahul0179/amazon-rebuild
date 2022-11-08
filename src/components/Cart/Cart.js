5bcf5e55ee9856d811415cf5021bee4b96786df4
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-11-08 20:48:22 +0600

    price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    let total = 0;
+    for (const product of cart) {
+
+        total = total + product.price;
+
+    }
     return (
-        <div>
+        <div className='cart'>
             <h2>Order summery</h2>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price : ${total} </p>
+            <p>Total Shipping : </p>
+            <p>Tax</p>
+            <h5>Grand Total</h5>
 
 
         </div>

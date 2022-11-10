46abe84034eeb6e6b530bfbf2ec1f2a999ef65c2
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-11-10 23:42:23 +0600

    tax add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,13 +11,14 @@ const Cart = (props) => {
         shipping = shipping + product.shipping
 
     }
+    const tax = (total * 0.1).toFixed(2);
     return (
         <div className='cart'>
             <h2>Order summery</h2>
             <p>Selected Items: {cart.length}</p>
             <p>Total price : ${total} </p>
-            <p>Total Shipping :{shipping} </p>
-            <p>Tax</p>
+            <p>Total Shipping :${shipping} </p>
+            <p>Tax :${tax}</p>
             <h5>Grand Total</h5>
 
 
